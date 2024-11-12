7d2cc7b7ecbb2b34f3d5bf7fa652ba11006164c7
Chen Chin fu <4b3g0043@office.stust.edu.tw>
2024-11-12 11:57:28 +0800

    遞迴與非遞迴乘階計算

--- Exam_hw/Exam_hw.cpp
+++ Exam_hw/Exam_hw.cpp
@@ -1,7 +1,26 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+int factorial(int n) {
+	if (n == 0) return 1;
+	else return n * factorial(n - 1);
+}
+
+int main() {
+	int num;
+
+	cout << "輸入一個正整數: ";
+	cin >> num;
+
+	// 非遞迴版本n!
+	int result = 1;
+	for (int i = 1; i <= num; i++) {
+		result *= i;
+		//result = result *i;
+	}
+	cout << "非遞迴:" <<  num << "! = " << result << endl;
+
+	// 遞迴版本n!
+	result = factorial(num);
+	cout << "遞迴:" << num << "! = " << result << endl;
 }
