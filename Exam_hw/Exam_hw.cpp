28a22f73a352aa38ae0ee8b14ed53e98a705d6df
Chen Chin fu <4b3g0043@office.stust.edu.tw>
2024-11-12 11:41:27 +0800

    三種迴圈運用

--- Exam_hw/Exam_hw.cpp
+++ Exam_hw/Exam_hw.cpp
@@ -1,7 +1,38 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+int main() {
+	int n,i,sum = 0;
+	cout << "請輸入一個正整數n :";
+	cin >> n;
+
+	//for loop
+	for (int i = 0; i <= n; i++) {
+		if (i % 2 == 0) sum += i;
+	}
+
+	sum = 0;
+	for (int i = 2; i < n; i += 2) {
+		sum += i;
+	}
+	cout << "for loop:1~" << n << "之間所有偶數和為: " << sum << endl;
+
+	// while loop
+	i = 1;
+	sum = 0;
+	while (i <= n) {
+		if (i % 2 == 0) sum += i;
+		i++;
+	}
+	cout << "while loop:1~" << n << "之間所有偶數和為: " << sum << endl;
+
+
+	// do-while loop
+	i = 2;
+	sum = 0;
+	do {
+		sum += i;
+		i += 2;
+	} while (i <= n);
+	cout << "do-while loop:1~" << n << "之間所有偶數和為: " << sum << endl;
 }
